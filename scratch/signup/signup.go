28f21306f0a5b38d7ff1362adbcb2368ade9172a
Tintinnabulate <tintinn@abulate>
2018-03-25 11:36:37 +0100

    Refactor emailing code

--- scratch/signup/signup.go
+++ scratch/signup/signup.go
@@ -16,6 +16,8 @@ import (
 )
 
 type Configuration struct {
+	SiteName     string
+	SiteDomain   string
 	SMTPServer   string
 	SMTPUsername string
 	SMTPPassword string
@@ -54,14 +56,21 @@ func emailCode(recipient, code string) {
 	server := config.SMTPServer
 	from := mail.Address{"", config.SMTPUsername}
 	to := mail.Address{"", recipient}
-	subj := "Verification Code"
+	subj := fmt.Sprintf("%s Verification Code", config.SiteName)
 	body := fmt.Sprintf("Code: %s", code)
+	sendEmail(server, from, to, subj, body)
+}
 
-	// Setup headers
+func makeEmailHeaders(from, to mail.Address, subject string) map[string]string {
 	headers := make(map[string]string)
 	headers["From"] = from.String()
 	headers["To"] = to.String()
-	headers["Subject"] = subj
+	headers["Subject"] = subject
+	return headers
+}
+
+func sendEmail(server string, from, to mail.Address, subject, body string) {
+	headers := makeEmailHeaders(from, to, subject)
 
 	// Setup message
 	message := ""
@@ -83,49 +92,40 @@ func emailCode(recipient, code string) {
 		ServerName:         host,
 	}
 
-	// Here is the key, you need to call tls.Dial instead of smtp.Dial
-	// for smtp servers running on 465 that require an ssl connection
+	// For smtp servers running on 465 that require an ssl connection
 	// from the very beginning (no starttls)
 	conn, err := tls.Dial("tcp", servername, tlsconfig)
 	if err != nil {
 		log.Panic(err)
 	}
-
 	c, err := smtp.NewClient(conn, host)
 	if err != nil {
 		log.Panic(err)
 	}
-
 	// Auth
 	if err = c.Auth(auth); err != nil {
 		log.Panic(err)
 	}
-
 	// To && From
 	if err = c.Mail(from.Address); err != nil {
 		log.Panic(err)
 	}
-
 	if err = c.Rcpt(to.Address); err != nil {
 		log.Panic(err)
 	}
-
 	// Data
 	w, err := c.Data()
 	if err != nil {
 		log.Panic(err)
 	}
-
 	_, err = w.Write([]byte(message))
 	if err != nil {
 		log.Panic(err)
 	}
-
 	err = w.Close()
 	if err != nil {
 		log.Panic(err)
 	}
-
 	c.Quit()
 }
 
