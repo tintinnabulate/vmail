091d03461a44bfbffb26f980aa4220ed9853d42f
Tintinnabulate <tintinn@abulate>
2018-04-20 21:47:03 +0100

    Refactor - moving db code into db_operations.db

--- scratch/signup/db_operations.go
+++ scratch/signup/db_operations.go
@@ -2,79 +2,110 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"time"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
-// getDB opens a database connection with file dbName.db
-func getDB(dbFilename string) *sql.DB {
-	db, err := sql.Open("sqlite3", dbFilename)
-	checkErr(err)
-	return db
-}
+var (
+	sqlFalse      = "\x00"
+	sqlTrue       = "\x01"
+	sqlConnection = "root:banana123@tcp(127.0.0.1:3306)/loldongs?parseTime=true"
+)
 
-func (db *sql.DB) insertUser(username string) {
-	// insert
-	stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
-	checkErr(err)
-
-	res, err := stmt.Exec("foobar", "teamname", "2018-04-04")
-	checkErr(err)
-}
-
-func (db *sql.DB) updateUsername(id int, username string) int {
-	// update
-	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
-	checkErr(err)
-
-	res, err = stmt.Exec(username, id)
-	checkErr(err)
-
-	affect, err := res.RowsAffected()
-	checkErr(err)
-	return affected
-}
-
-func (db *sql.DB) getAllUsers() *sql.Rows {
-	// query
-	rows, err := db.Query("SELECT * FROM userinfo")
-	checkErr(err)
-	return rows
-}
-
-func (rs *sql.Rows) printUsers() {
-
-	var uid int
-	var username string
-	var department string
-	var created time.Time
-
-	for rs.Next() {
-		err = rs.Scan(&uid, &username, &department, &created)
-		checkErr(err)
-		fmt.Println("uid:", uid)
-		fmt.Println("username:", username)
-		fmt.Println("department:", department)
-		fmt.Println("created:", created.String())
-	}
-	rs.Close()
-}
-
-func (db *sql.DB) deleteAllUser() int {
-	// delete
-	res, err = db.Exec("DELETE FROM userinfo")
-	checkErr(err)
-
-	affect, err = res.RowsAffected()
-	checkErr(err)
-	return affect
-
-	// db.Close()
+func getSQLConnection() (*sql.DB, error) {
+	db, err := sql.Open("mysql", sqlConnection)
+	return db, err
 }
 
 func checkErr(err error) {
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+}
+
+func (s *signup) insert(db *sql.DB) (int64, error) {
+	stmt, err := db.Prepare("INSERT INTO " +
+		"signups(email, verification_code, is_verified) " +
+		"VALUES(?, ?, ?);")
+	checkErr(err)
+	res, err := stmt.Exec(
+		s.Email,
+		s.VerificationCode,
+		s.IsVerified)
+	checkErr(err)
+	lastId, err := res.LastInsertId()
+	return lastId, err
+}
+
+func isValidVerificationCode(db *sql.DB, code string) (bool, string, signup) {
+	// get signup using code
+	stmt, err := db.Prepare(
+		`SELECT 
+		id, creation_timestamp, email, 
+		verification_code, is_verified 
+		FROM signups WHERE verification_code = ?;`)
+	checkErr(err)
+	defer stmt.Close()
+	var maybeSignup signup
+	// populate signup
+	err = stmt.QueryRow(code).Scan(
+		&maybeSignup.ID,
+		&maybeSignup.CreationTimestamp,
+		&maybeSignup.Email,
+		&maybeSignup.VerificationCode,
+		&maybeSignup.IsVerified)
+	switch {
+	// no match
+	case err == sql.ErrNoRows:
+		return false,
+			"No such verification code",
+			maybeSignup
+	case err != nil:
+		log.Fatal(err)
+		return false,
+			"",
+			maybeSignup
+	// match
+	default:
+		// already verified, needs to continue
+		if string(maybeSignup.IsVerified) == sqlTrue {
+			return false,
+				"You are already verified. " +
+					"Please continue with the Registration process: /signup",
+				maybeSignup
+		} else {
+			return true,
+				"You are now verified. " +
+					"Please continue with the Registration process: /signup",
+				maybeSignup
+		}
+	}
+}
+
+func (s *signup) verify(db *sql.DB) error {
+	stmt2, err := db.Prepare(
+		`UPDATE signups
+		SET is_verified = true
+	    WHERE id = ? 
+		AND is_verified = false;`)
+	defer stmt2.Close()
+	_, err = stmt2.Exec(s.ID)
+	return err
+}
+
+// TODO fix query - not enough args
+func fetchSignup(db *sql.DB, id int64) (signup, error) {
+	var s signup
+	err := db.QueryRow("SELECT creation_timestamp, email, verification_code FROM signups WHERE id = ?", id).Scan(
+		&s.CreationTimestamp,
+		&s.Email,
+		&s.VerificationCode)
+	return s, err
+}
+
+func deleteSignup(db *sql.DB, id int64) error {
+	stmt, err := db.Prepare("DELETE FROM signups WHERE id = ?")
+	checkErr(err)
+	_, err = stmt.Exec(id)
+	return err
 }
--- scratch/signup/signup.go
+++ scratch/signup/signup.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"crypto/rand"
-	"database/sql"
 	"encoding/json"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"log"
 	"net/http"
@@ -24,11 +22,7 @@ type configuration struct {
 
 var (
 	config    configuration
-	db        *sql.DB
 	validPath = regexp.MustCompile("^/verify/([a-zA-Z0-9]+)$")
-	LOCATION  = time.UTC
-	sqlFalse  = "\x00"
-	sqlTrue   = "\x01"
 )
 
 func init() {
@@ -41,17 +35,6 @@ func init() {
 	http.HandleFunc("/signup/", signupHandler)
 	http.HandleFunc("/signup_submit/", signupSubmitHandler)
 	http.HandleFunc("/verify/", verifyHandler)
-}
-
-// Datetime is a utility function for making dates with the same
-// location, 0 hours, 0 mins, 0 secs, 0 nanosecs.
-func Datetime(year int, month time.Month, day int) time.Time {
-	return time.Date(year, month, day, 0, 0, 0, 0, LOCATION)
-}
-
-// Now returns the time right now when it is called
-func Now() time.Time {
-	return time.Now().In(LOCATION)
 }
 
 type signup struct {
@@ -80,16 +63,14 @@ func signupSubmitHandler(w http.ResponseWriter, r *http.Request) {
 		Email:            []byte(email),
 		VerificationCode: randToken(),
 		IsVerified:       []byte(sqlFalse)}
-	db, err := sql.Open("mysql",
-		"root:banana123@tcp(127.0.0.1:3306)/loldongs?parseTime=true")
+	db, err := getSQLConnection()
 	checkErr(err)
 	defer db.Close()
-	lastId, err := s.insert(db)
+	_, err = s.insert(db)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(lastId)
 	emailCode(string(s.Email), s.VerificationCode)
 	http.Redirect(w, r, "/signup/", http.StatusFound)
 }
@@ -102,8 +83,7 @@ func verifyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println(m)
 	code := m[1]
-	db, err := sql.Open("mysql",
-		"root:banana123@tcp(127.0.0.1:3306)/loldongs?parseTime=true")
+	db, err := getSQLConnection()
 	checkErr(err)
 	defer db.Close()
 	isValid, reason, maybeSignup := isValidVerificationCode(db, code)
@@ -118,97 +98,4 @@ func verifyHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func (s *signup) insert(db *sql.DB) (int64, error) {
-	stmt, err := db.Prepare("INSERT INTO " +
-		"signups(email, verification_code, is_verified) " +
-		"VALUES(?, ?, ?);")
-	checkErr(err)
-	res, err := stmt.Exec(
-		s.Email,
-		s.VerificationCode,
-		s.IsVerified)
-	checkErr(err)
-	lastId, err := res.LastInsertId()
-	return lastId, err
-}
-
-func isValidVerificationCode(db *sql.DB, code string) (bool, string, signup) {
-	// get signup using code
-	stmt, err := db.Prepare(
-		`SELECT 
-		id, creation_timestamp, email, 
-		verification_code, is_verified 
-		FROM signups WHERE verification_code = ?;`)
-	checkErr(err)
-	defer stmt.Close()
-	var maybeSignup signup
-	// populate signup
-	err = stmt.QueryRow(code).Scan(
-		&maybeSignup.ID,
-		&maybeSignup.CreationTimestamp,
-		&maybeSignup.Email,
-		&maybeSignup.VerificationCode,
-		&maybeSignup.IsVerified)
-	switch {
-	// no match
-	case err == sql.ErrNoRows:
-		return false,
-			"No such verification code",
-			maybeSignup
-	case err != nil:
-		log.Fatal(err)
-		return false,
-			"",
-			maybeSignup
-	// match
-	default:
-		// already verified, needs to continue
-		if string(maybeSignup.IsVerified) == sqlTrue {
-			return false,
-				"You are already verified. " +
-					"Please continue with the Registration process: /signup",
-				maybeSignup
-		} else {
-			return true,
-				"You are now verified. " +
-					"Please continue with the Registration process: /signup",
-				maybeSignup
-		}
-	}
-}
-
-func (s *signup) verify(db *sql.DB) error {
-	stmt2, err := db.Prepare(
-		`UPDATE signups
-		SET is_verified = true
-	    WHERE id = ? 
-		AND is_verified = false;`)
-	defer stmt2.Close()
-	_, err = stmt2.Exec(s.ID)
-	return err
-}
-
-// TODO fix query - not enough args
-func fetchSignup(db *sql.DB, id int64) (signup, error) {
-	var s signup
-	err := db.QueryRow("SELECT creation_timestamp, email, verification_code FROM signups WHERE id = ?", id).Scan(
-		&s.CreationTimestamp,
-		&s.Email,
-		&s.VerificationCode)
-	return s, err
-}
-
-func deleteSignup(db *sql.DB, id int64) error {
-	stmt, err := db.Prepare("DELETE FROM signups WHERE id = ?")
-	checkErr(err)
-	_, err = stmt.Exec(id)
-	return err
 }
