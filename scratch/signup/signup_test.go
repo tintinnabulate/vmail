721de38a3fc038db4fdb81b7ddf50d14dc6a185d
Tintinnabulate <tintinn@abulate>
2018-05-14 16:36:58 +0100

    Update comments

--- scratch/signup/signup_test.go
+++ scratch/signup/signup_test.go
@@ -21,6 +21,7 @@ func CreateContextHandlerToHttpHandler(ctx context.Context) ContextHandlerToHand
 	}
 }
 
+// TestCreateSignupEndpoint tests that we can create a signup
 func TestCreateSignupEndpoint(t *testing.T) {
 	LoadConfig()
 
@@ -42,33 +43,35 @@ func TestCreateSignupEndpoint(t *testing.T) {
 	})
 }
 
+// TestCreateAndVerifyAndCheckSignupEndpoint tests that we can create a signup, verify it, and then check that it is verified
 func TestCreateAndVerifyAndCheckSignupEndpoint(t *testing.T) {
 	LoadConfig()
 
 	ctx, _, _ := aetest.NewContext()
 
-	c.Convey("When you want to do foo", t, func() {
+	c.Convey("When creating a signup for email address 'lolz'", t, func() {
 		r := CreateHandler(CreateContextHandlerToHttpHandler(ctx))
-		record := httptest.NewRecorder()
-		record2 := httptest.NewRecorder()
-		record3 := httptest.NewRecorder()
+		record := httptest.NewRecorder()  // records the 'create signup' response
+		record2 := httptest.NewRecorder() // records the 'verify signup' response
+		record3 := httptest.NewRecorder() // records the 'check signup is verified' repsonse
 
 		req, err := http.NewRequest("POST", "/signup/lolz", nil)
 		c.So(err, c.ShouldBeNil)
 
-		c.Convey("It should return a 200 response", func() {
+		c.Convey("It should succeed", func() {
 
 			r.ServeHTTP(record, req)
 			c.So(record.Code, c.ShouldEqual, 200)
 			c.So(fmt.Sprint(record.Body), c.ShouldEqual, `{"address":"lolz","success":true,"note":""}
 `)
 
+            // Look up code sent to 'lolz'
 			code, _ := GetSignupCode(ctx, "lolz")
 
 			req2, err2 := http.NewRequest("GET", fmt.Sprintf("/verify/%s", code), nil)
 			c.So(err2, c.ShouldBeNil)
 
-			c.Convey("Should be 200", func() {
+			c.Convey("Verifying the code sent to 'lolz' should succeed", func() {
 
 				r.ServeHTTP(record2, req2)
 				c.So(record2.Code, c.ShouldEqual, 200)
@@ -78,7 +81,7 @@ func TestCreateAndVerifyAndCheckSignupEndpoint(t *testing.T) {
 				req3, err3 := http.NewRequest("GET", "/signup/lolz", nil)
 				c.So(err3, c.ShouldBeNil)
 
-				c.Convey("Should be 200", func() {
+				c.Convey("Checking email 'lolz' is verified should succeed", func() {
 					r.ServeHTTP(record3, req3)
 					c.So(record3.Code, c.ShouldEqual, 200)
 					c.So(fmt.Sprint(record3.Body), c.ShouldEqual, `{"address":"lolz","success":true,"note":""}
@@ -90,19 +93,20 @@ func TestCreateAndVerifyAndCheckSignupEndpoint(t *testing.T) {
 	})
 }
 
+// TestVerifySignupEndpoint tests that verifying a non-existent code produces a JSON response where "success": false .
 func TestVerifySignupEndpoint(t *testing.T) {
 	LoadConfig()
 
 	ctx, _, _ := aetest.NewContext()
 
-	c.Convey("When you want to do foo", t, func() {
+	c.Convey("When you try and verify a non-existent code", t, func() {
 		r := CreateHandler(CreateContextHandlerToHttpHandler(ctx))
 		record := httptest.NewRecorder()
 
 		req, err := http.NewRequest("GET", "/verify/lolz", nil)
 		c.So(err, c.ShouldBeNil)
 
-		c.Convey("It should return a 200 response", func() {
+		c.Convey("It should return a 200 response, but fail", func() {
 			r.ServeHTTP(record, req)
 			c.So(record.Code, c.ShouldEqual, 200)
 			c.So(fmt.Sprint(record.Body), c.ShouldEqual, `{"code":"lolz","Success":false,"Note":"no such verification code"}
