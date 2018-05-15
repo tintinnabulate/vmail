fc8ba5f00892b2ccdf953b6320d424f9e90a870b
Tintinnabulate <tintinn@abulate>
2018-05-15 21:19:02 +0100

    Begin writing registration

--- scratch/register/register.go
+++ scratch/register/register.go
@@ -1,101 +1,24 @@
 package main
 
 import (
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
+
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/mail"
 )
 
-func Monkeys(ctx context.Context, w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	fmt.Fprintf(w, "banana: %s", params["poop"])
+func PostRegistrationHandler(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hoi")
 }
 
-func VerifyCodeEndpoint(ctx context.Context, w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	w.Header().Set("Content-Type", "application/json")
-	var verification Verification
-	verification.Code = params["code"]
-	err := MarkVerified(ctx, verification.Code)
-	verification.Success = true
-	verification.Note = ""
-	if err != nil {
-		verification.Success = false
-		verification.Note = err.Error()
-	}
-	json.NewEncoder(w).Encode(verification)
-}
-
-func ComposeVerificationEmail(address, code string) *mail.Message {
-	return &mail.Message{
-		Sender:  "[DONUT REPLY] Admin <donotreply@seraphic-lock-199316.appspotmail.com>",
-		To:      []string{address},
-		Subject: "Your verification code",
-		Body:    fmt.Sprintf(emailBody, code),
-	}
-}
-
-func EmailVerificationCode(ctx context.Context, address, code string) error {
-	msg := ComposeVerificationEmail(address, code)
-	return mail.Send(ctx, msg)
-}
-
-func CreateSignupEndpoint(ctx context.Context, w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	w.Header().Set("Content-Type", "application/json")
-	var email Email
-	email.Address = params["email"]
-	code := ""
-	for {
-		code = randToken()
-		codeIsOkayToUse, err := IsCodeFree(ctx, code)
-		checkErr(err)
-		if codeIsOkayToUse {
-			break
-		}
-	}
-	if err := EmailVerificationCode(ctx, email.Address, code); err != nil {
-		email.Success = false
-		email.Note = err.Error()
-		json.NewEncoder(w).Encode(email)
-		return
-	}
-	_, err := AddSignup(ctx, email.Address, code)
-	if err != nil {
-		email.Success = false
-		email.Note = err.Error()
-	} else {
-		email.Success = true
-	}
-	json.NewEncoder(w).Encode(email)
-}
-
-func CheckSignupEndpoint(ctx context.Context, w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	w.Header().Set("Content-Type", "application/json")
-	var email Email
-	email.Address = params["email"]
-	exists, err := CheckSignup(ctx, email.Address)
-	if err != nil {
-		email.Success = false
-		email.Note = err.Error()
-		json.NewEncoder(w).Encode(email)
-		return
-	}
-	if !exists {
-		email.Success = false
-	} else {
-		email.Success = true
-	}
-	json.NewEncoder(w).Encode(email)
+func GetRegistrationHandler(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hoi")
 }
 
 type configuration struct {
@@ -107,29 +30,10 @@ type configuration struct {
 	ProjectID    string
 }
 
-type Email struct {
-	Address string `json:"address"`
-	Success bool   `json:"success"`
-	Note    string `json:"note"`
-}
-
-type Verification struct {
-	Code    string `json:"code"`
-	Success bool   `json:success"`
-	Note    string `json:note"`
-}
-
 var (
 	config    configuration
 	appRouter mux.Router
 )
-
-const emailBody = `
-Code: %s
-
-Yours randomly,
-Bert.
-`
 
 func checkErr(err error) {
 	if err != nil {
@@ -144,12 +48,6 @@ func LoadConfig() {
 	config = configuration{}
 	err := decoder.Decode(&config)
 	checkErr(err)
-}
-
-func randToken() string {
-	b := make([]byte, 4)
-	rand.Read(b)
-	return fmt.Sprintf("%x", b)
 }
 
 /*
@@ -183,10 +81,8 @@ Creates my mux.Router. Uses f to convert ContextHandlerFunc's to HandlerFuncs.
 */
 func CreateHandler(f ContextHandlerToHandlerHOF) *mux.Router {
 	appRouter := mux.NewRouter()
-	appRouter.HandleFunc("/verify/{code}", f(VerifyCodeEndpoint)).Methods("GET")
-	appRouter.HandleFunc("/signup/{email}", f(CreateSignupEndpoint)).Methods("POST")
-	appRouter.HandleFunc("/signup/{email}", f(CheckSignupEndpoint)).Methods("GET")
-	appRouter.HandleFunc("/monkeys/{poop}", f(Monkeys)).Methods("GET")
+	appRouter.HandleFunc("/register", f(PostRegistrationHandler)).Methods("POST")
+	appRouter.HandleFunc("/register", f(GetRegistrationHandler)).Methods("GET")
 
 	return appRouter
 }
--- scratch/register/register_test.go
+++ scratch/register/register_test.go
@@ -9,8 +9,8 @@ import (
 	c "github.com/smartystreets/goconvey/convey"
 
 	"golang.org/x/net/context"
-	//"google.golang.org/appengine"
 	"google.golang.org/appengine/aetest"
+	//"google.golang.org/appengine"
 )
 
 func CreateContextHandlerToHttpHandler(ctx context.Context) ContextHandlerToHandlerHOF {
@@ -21,100 +21,42 @@ func CreateContextHandlerToHttpHandler(ctx context.Context) ContextHandlerToHand
 	}
 }
 
-func TestMonkeys(t *testing.T) {
+func TestPostRegistrationHandler(t *testing.T) {
 	LoadConfig()
 
 	ctx, _, _ := aetest.NewContext()
 
-	c.Convey("When you want to do foo", t, func() {
+	c.Convey("When user tries to register with an unverified email address", t, func() {
 		r := CreateHandler(CreateContextHandlerToHttpHandler(ctx))
 		record := httptest.NewRecorder()
 
-		req, err := http.NewRequest("GET", "/monkeys/dong", nil)
+		req, err := http.NewRequest("POST", "/register", nil)
+		c.So(err, c.ShouldBeNil)
+
+		c.Convey("It should return a 200 response, but suggest /signup", func() {
+			r.ServeHTTP(record, req)
+			c.So(record.Code, c.ShouldEqual, 200)
+			c.So(fmt.Sprint(record.Body), c.ShouldEqual, "Please sign up first /signup")
+		})
+	})
+}
+
+func TestGetRegistrationHandler(t *testing.T) {
+	LoadConfig()
+
+	ctx, _, _ := aetest.NewContext()
+
+	c.Convey("When user visits the registration page", t, func() {
+		r := CreateHandler(CreateContextHandlerToHttpHandler(ctx))
+		record := httptest.NewRecorder()
+
+		req, err := http.NewRequest("GET", "/register", nil)
 		c.So(err, c.ShouldBeNil)
 
 		c.Convey("It should return a 200 response", func() {
 			r.ServeHTTP(record, req)
 			c.So(record.Code, c.ShouldEqual, 200)
-			c.So(fmt.Sprint(record.Body), c.ShouldEqual, "banana: dong")
+			c.So(fmt.Sprint(record.Body), c.ShouldEqual, "hoi")
 		})
 	})
 }
-
-func TestCreateSignupEndpoint(t *testing.T) {
-	LoadConfig()
-
-	ctx, _, _ := aetest.NewContext()
-
-	c.Convey("When you want to do foo", t, func() {
-		r := CreateHandler(CreateContextHandlerToHttpHandler(ctx))
-		record := httptest.NewRecorder()
-
-		req, err := http.NewRequest("POST", "/signup/lolz", nil)
-		c.So(err, c.ShouldBeNil)
-
-		c.Convey("It should return a 200 response", func() {
-			r.ServeHTTP(record, req)
-			c.So(record.Code, c.ShouldEqual, 200)
-			c.So(fmt.Sprint(record.Body), c.ShouldEqual, `{"address":"lolz","success":true,"note":""}
-`)
-		})
-	})
-}
-
-func TestCreateAndCheckSignupEndpoint(t *testing.T) {
-	LoadConfig()
-
-	ctx, _, _ := aetest.NewContext()
-
-	c.Convey("When you want to do foo", t, func() {
-		r := CreateHandler(CreateContextHandlerToHttpHandler(ctx))
-		record := httptest.NewRecorder()
-		record2 := httptest.NewRecorder()
-
-		req, err := http.NewRequest("POST", "/signup/lolz", nil)
-		c.So(err, c.ShouldBeNil)
-
-		c.Convey("It should return a 200 response", func() {
-
-			r.ServeHTTP(record, req)
-			c.So(record.Code, c.ShouldEqual, 200)
-			c.So(fmt.Sprint(record.Body), c.ShouldEqual, `{"address":"lolz","success":true,"note":""}
-`)
-
-			req2, err2 := http.NewRequest("GET", "/signup/lolz", nil)
-			c.So(err2, c.ShouldBeNil)
-
-			c.Convey("It should return a 200 response", func() {
-				r.ServeHTTP(record2, req2)
-				c.So(record2.Code, c.ShouldEqual, 200)
-				c.So(fmt.Sprint(record2.Body), c.ShouldEqual, `{"address":"lolz","success":false,"note":""}
-`)
-			})
-		})
-
-	})
-}
-
-func TestVerifySignupEndpoint(t *testing.T) {
-	LoadConfig()
-
-	ctx, _, _ := aetest.NewContext()
-
-	c.Convey("When you want to do foo", t, func() {
-		r := CreateHandler(CreateContextHandlerToHttpHandler(ctx))
-		record := httptest.NewRecorder()
-
-		req, err := http.NewRequest("GET", "/verify/lolz", nil)
-		c.So(err, c.ShouldBeNil)
-
-		c.Convey("It should return a 200 response", func() {
-			r.ServeHTTP(record, req)
-			c.So(record.Code, c.ShouldEqual, 200)
-			c.So(fmt.Sprint(record.Body), c.ShouldEqual, `{"code":"lolz","Success":false,"Note":"no such verification code"}
-`)
-		})
-	})
-}
-
-// TODO test adding a valid signup and look for that code
