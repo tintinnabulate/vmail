7d4db89c7cde19ac6999247c06e636bd8c9de1f7
Tintinnabulate <tintinn@abulate>
2018-05-16 22:43:49 +0100

    Add comment

--- scratch/register/register.go
+++ scratch/register/register.go
@@ -87,6 +87,7 @@ func checkErr(err error) {
 	}
 }
 
+// TODO: this will need adapting to whatever format we request for Sobriety_Date and Birth_Date
 func timeConverter(value string) reflect.Value {
 	tstamp, _ := strconv.ParseInt(value, 10, 64)
 	return reflect.ValueOf(time.Unix(tstamp, 0))
